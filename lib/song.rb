d54ea73eec888f2107ca84c0136233f6acc3b723
Andrew Bay <daddnbay@yahoo.com>
2019-07-13 19:58:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   
   @@count = 0
+  @@genres = []
   @@artists = []
   
   attr_accessor :name, :artist, :genre
@@ -10,11 +11,16 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre
     @@artists << artist
   end
   
   def self.count
     @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
   end
   
   def self.artists
